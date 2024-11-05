2df55fe317925a3237366bc4281d964eb14255f4
Simrat Grewal <simrats169169@gmail.com>
2024-11-04 20:23:57 -0800

    Network: Add more endpoints

--- src/network_handler.py
+++ src/network_handler.py
@@ -1,5 +1,7 @@
 import psutil
 import time
+import socket
+
 
 class Network:
     @staticmethod
@@ -9,17 +11,18 @@ class Network:
         """
         net_io = psutil.net_io_counters()
         return {
-            'bytes_sent': net_io.bytes_sent,
-            'bytes_received': net_io.bytes_recv,
-            'packets_sent': net_io.packets_sent,
-            'packets_received': net_io.packets_recv
+            "bytes_sent": net_io.bytes_sent,
+            "bytes_received": net_io.bytes_recv,
+            "packets_sent": net_io.packets_sent,
+            "packets_received": net_io.packets_recv,
         }
+
     @staticmethod
     def bytes_convert(bytes_size):
         """
         Converts bytes to a human-readable format
         """
-        units = ['B/s', 'KB/s', 'MB/s', 'GB/s', 'TB/s']
+        units = ["B/s", "KB/s", "MB/s", "GB/s", "TB/s"]
         size = bytes_size
         unit = 0
         while size >= 1024 and unit < len(units) - 1:
@@ -43,13 +46,17 @@ class Network:
                 current_net_io = psutil.net_io_counters()
 
                 # Calculate the total bytes per second
-                sent_per_second = (current_net_io.bytes_sent - previous_net_io.bytes_sent) / interval
-                recv_per_second = (current_net_io.bytes_recv - previous_net_io.bytes_recv) / interval
+                sent_per_second = (
+                    current_net_io.bytes_sent - previous_net_io.bytes_sent
+                ) / interval
+                recv_per_second = (
+                    current_net_io.bytes_recv - previous_net_io.bytes_recv
+                ) / interval
 
                 sent_human = Network.bytes_convert(sent_per_second)
                 recv_human = Network.bytes_convert(recv_per_second)
 
-                print(f'{sent_human} {recv_human}')
+                print(f"{sent_human} {recv_human}")
 
                 previous_net_io = current_net_io
 
@@ -61,19 +68,52 @@ class Network:
         previous_net_io = psutil.net_io_counters()
         time.sleep(interval)
         current_net_io = psutil.net_io_counters()
-        sent_per_second = (current_net_io.bytes_sent - previous_net_io.bytes_sent) / interval
-        recv_per_second = (current_net_io.bytes_recv - previous_net_io.bytes_recv) / interval
-        monitored_traffic = {
-            "sent": sent_per_second,
-            "recv": recv_per_second
-        }
+        sent_per_second = (
+            current_net_io.bytes_sent - previous_net_io.bytes_sent
+        ) / interval
+        recv_per_second = (
+            current_net_io.bytes_recv - previous_net_io.bytes_recv
+        ) / interval
+        monitored_traffic = {"sent": sent_per_second, "recv": recv_per_second}
         return monitored_traffic
+
+    @staticmethod
+    def get_primary_ipv4():
+        hostname = socket.gethostname()
+        ipv4_address = socket.gethostbyname(hostname)
+        return ipv4_address
+
+    @staticmethod
+    def get_primary_ipv6():
+        hostname = socket.gethostname()
+        ipv6_info = socket.getaddrinfo(hostname, None, socket.AF_INET6)
+        if ipv6_info:
+            return ipv6_info[0][4][0]
+        return None
+
+    def get_primary_connection_type():
+        connections = psutil.net_if_addrs()
+        active_connections = psutil.net_if_stats()
+
+        for interface in connections:
+            if active_connections[interface].isup:  # Check if interface is active
+                # Determine connection type based on naming conventions
+                if "wlan" in interface or "Wi-Fi" in interface or "wifi" in interface:
+                    return "WiFi"
+                elif "eth" in interface or "en" in interface:
+                    return "Ethernet"
+                else:
+                    return "Unknown"
+
 
 def main():
 
     bandwidth = Network.get_bandwidth_usage()
-    print(f"Total Bytes Sent: {bandwidth['bytes_sent']}, Total Bytes Received: {bandwidth['bytes_received']}")
+    print(
+        f"Total Bytes Sent: {bandwidth['bytes_sent']}, Total Bytes Received: {bandwidth['bytes_received']}"
+    )
     Network.monitor_realtime_total_traffic(interval=1)
-    
+
+
 if __name__ == "__main__":
     main()
--- src/server.py
+++ src/server.py
@@ -20,6 +20,7 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
+
 @app.get("/processes", response_model=list)
 def get_all_processes():
     """
@@ -27,6 +28,7 @@ def get_all_processes():
     """
     processes = Process.list_all_processes()
     return processes
+
 
 @app.get("/processes/{pid}", response_model=dict)
 def get_process_by_pid(pid: int):
@@ -38,16 +40,16 @@ def get_process_by_pid(pid: int):
         return process
     else:
         raise HTTPException(status_code=404, detail=f"Process with PID {pid} not found")
-    
+
+
 @app.post("/killprocess/{pid}")
-def post_kill_process(pid:int):
-    result_string= Process.kill_process(pid)
-    res = {
-        "res" : result_string
-    }
+def post_kill_process(pid: int):
+    result_string = Process.kill_process(pid)
+    res = {"res": result_string}
     return res
 
-#CPU
+
+# CPU
 @app.get("/cpu/all")
 def get_all_cpu_data():
     cpu_data = {
@@ -58,43 +60,52 @@ def get_all_cpu_data():
         "load_average": CPU.get_load_average(),
         "core_utilization": CPU.get_core_utilization(),
         "cpu_temperature": CPU.get_cpu_temperature(),
-        "cpu_times": CPU.get_cpu_times()
+        "cpu_times": CPU.get_cpu_times(),
     }
     return cpu_data
+
 
 @app.get("/cpu/usage")
 def cpu_usage():
     return {"cpu_usage": CPU.get_cpu_usage()}
 
+
 @app.get("/cpu/per_cpu_usage")
 def per_cpu_usage():
     return {"per_cpu_usage": CPU.get_per_cpu_usage()}
+
 
 @app.get("/cpu/frequency")
 def cpu_frequency():
     return {"cpu_frequency": CPU.get_cpu_frequency()}
 
+
 @app.get("/cpu/count")
 def cpu_count():
     return {"cpu_count": CPU.get_cpu_count()}
+
 
 @app.get("/cpu/load_average")
 def load_average():
     return {"load_average": CPU.get_load_average()}
 
+
 @app.get("/cpu/core_utilization")
 def core_utilization():
     return {"core_utilization": CPU.get_core_utilization()}
+
 
 @app.get("/cpu/temperature")
 def cpu_temperature():
     return {"cpu_temperature": CPU.get_cpu_temperature()}
 
+
 @app.get("/cpu/times")
 def cpu_times():
     return {"cpu_times": CPU.get_cpu_times()}
 
-#MEMORY
+
+# MEMORY
 @app.get("/memory/all")
 def get_all_memory_data():
     memory_data = {
@@ -103,38 +114,51 @@ def get_all_memory_data():
         "memory_percent": Memory.get_memory_percent(),
         "memory_usage": Memory.get_memory_usage(),
         "memory_available": Memory.get_memory_available(),
-        "memory_total": Memory.get_memory_total()
+        "memory_total": Memory.get_memory_total(),
     }
     return memory_data
+
 
 @app.get("/memory/virtual")
 def virtual_memory():
     return {"virtual_memory": Memory.get_virtual_memory()}
 
+
 @app.get("/memory/swap")
 def swap_memory():
     return {"swap_memory": Memory.get_swap_memory()}
+
 
 @app.get("/memory/percent")
 def memory_percent():
     return {"memory_percent": Memory.get_memory_percent()}
 
+
 @app.get("/memory/usage")
 def memory_usage():
     return {"memory_usage": Memory.get_memory_usage()}
+
 
 @app.get("/memory/available")
 def memory_available():
     return {"memory_available": Memory.get_memory_available()}
 
+
 @app.get("/memory/total")
 def memory_total():
     return {"memory_total": Memory.get_memory_total()}
 
-#NETWORK HANDLER
-@app.get("/network/bandwidth")
+
+# NETWORK HANDLER
+@app.get("/network/all")
 def get_bandwidth_usage():
-    return {"bandwidth_usage": Network.get_bandwidth_usage()}
+    return {
+        "bandwidth_usage": Network.get_bandwidth_usage(),
+        "ipv4": Network.get_primary_ipv4(),
+        "ipv6": Network.get_primary_ipv6(),
+        "type": Network.get_primary_connection_type(),
+    }
+
 
 @app.get("/disk/all")
 def get_all_disk_data(path: str = "/"):
@@ -147,9 +171,10 @@ def get_all_disk_data(path: str = "/"):
         "usage": Disk.get_disk_usage(path),
         "io_counters": Disk.get_disk_io_counters(),
         "io_counters_per_disk": Disk.get_disk_io_counters_per_disk(),
-        "active_time": Disk.get_disk_active_time_percentage()
+        "active_time": Disk.get_disk_active_time_percentage(),
     }
     return disk_data
+
 
 @app.get("/gpu/all")
 def get_all_gpu_data():
@@ -159,9 +184,10 @@ def get_all_gpu_data():
         "temperature": GPU.get_gpu_temperature(),
         "count": GPU.get_gpu_count(),
         "details": GPU.get_gpu_details(),
-        "power_usage": GPU.get_gpu_power_usage()
+        "power_usage": GPU.get_gpu_power_usage(),
     }
     return gpu_data
+
 
 @app.get("/realtime")
 def get_all_realtime_data():
